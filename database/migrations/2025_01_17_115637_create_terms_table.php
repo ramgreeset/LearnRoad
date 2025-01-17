c1f4d47ba0ad1536c3d5d87434e2653d8a073e1e
Roman Artemyev <artemyev.ram@gmail.com>
2025-01-17 15:29:47 +0300

    init

---
+++ database/migrations/2025_01_17_115637_create_terms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('terms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('terms_id')->autoIncrement();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->string('links')->nullable();
+            $table->integer('grade')->default(1);
+            $table->string('tag')->nullable();
+            $table->integer('repeat_counter')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('terms');
+    }
+};
