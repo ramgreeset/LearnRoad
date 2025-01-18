3e5239850865dc53f390b4dca0f0646612be2429
Roman Artemyev <artemyev.ram@gmail.com>
2025-01-18 15:02:10 +0300

    Подключение tailwind. Создание компонентов header и footer

--- database/migrations/2025_01_17_115637_create_terms_table.php
+++ database/migrations/2025_01_17_115637_create_terms_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('terms', function (Blueprint $table) {
             $table->id();
-            $table->integer('terms_id')->autoIncrement();
             $table->string('title');
             $table->string('description')->nullable();
             $table->string('links')->nullable();
